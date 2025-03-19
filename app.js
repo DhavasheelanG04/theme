c1f5fc881e0aaef4c63ca71947b232b6a5fd1d18
DhavasheelanG04 <dhavajaya8283@gmail.com>
2025-03-20 00:04:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var body=document.querySelector("body");
+
+function col_black(){
+    body.className="";
+    body.classList.add("dark");
+}
+function col_white(){
+    body.className="";
+    body.classList.add("white");
+}
